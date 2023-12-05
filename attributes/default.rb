89dec741ea7aada6f801b6928714702cee3c60e1
Will Fisher <whfisher@alaska.edu>
2023-12-04 17:37:07 -0900

    Move package information into attributes
    Signed-off-by: Will Fisher <whfisher@alaska.edu>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 default['headscale']['server_url'] = "http://0.0.0.0:8080"
 default['headscale']['listen_addr'] = "0.0.0.0:8080"
 default['headscale']['grpc_listen_addr'] = "0.0.0.0:50443"
+
+default['headscale']['package_source'] = 'https://github.com/juanfont/headscale/releases/download/v0.22.3/headscale_0.22.3_linux_amd64.deb'
+default['headscale']['package_checksum'] = '2b45be5aa7b95c2512f57c83931ed3eb4f546fca5dfc771c7458a13392adb331'
